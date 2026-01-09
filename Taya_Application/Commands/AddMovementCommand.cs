bac9cd8d6da84cd0eaa176e483be4d42cfbf34fd
Julio Isael Rivera Ramirez <julio.riveraRamirez@outlook.com>
2026-01-09 06:23:59 +0100

    Comments Code refactor

--- Taya_Application/Commands/AddMovementCommand.cs
+++ Taya_Application/Commands/AddMovementCommand.cs
@@ -4,10 +4,17 @@
     using MediatR;
     using Taya_Helper.DTOs;
 
+    /// <summary>
+    /// Command to add new movement
+    /// </summary>
     public class AddMovementCommand : IRequest<Guid?>
     {
         public MovementDto Movement { get; set; }
 
+        /// <summary>
+        /// Main constructor
+        /// </summary>
+        /// <param name="entity"></param>
         public AddMovementCommand(MovementDto entity)
         {
             Movement = entity;
--- Taya_WebApi/Controllers/MovementController.cs
+++ Taya_WebApi/Controllers/MovementController.cs
@@ -10,6 +10,9 @@
     using Taya_Helper.DTOs;
     using Taya_Helper.Filters;
 
+    /// <summary>
+    /// Movement main controller including CRUD endpoints
+    /// </summary>
     [ApiController]
     [Route("api/Movements")]
     public class MovementController : Controller
@@ -17,12 +20,22 @@
         private readonly IMediator _mediator;
         private readonly ILogger _logger;
 
+        /// <summary>
+        /// Main constructor por DI
+        /// </summary>
+        /// <param name="mediator"></param>
+        /// <param name="logger"></param>
         public MovementController(IMediator mediator, ILogger logger)
         { 
             _logger = logger;
             _mediator = mediator;
         }
 
+        /// <summary>
+        /// Get a single movement by his Id
+        /// </summary>
+        /// <param name="id"></param>
+        /// <returns></returns>
         [HttpGet]
         [Route("{movementId}")]
         [ProducesResponseType(typeof(MovementDto), (int)HttpStatusCode.OK)]
@@ -42,6 +55,11 @@
             }
         }
 
+        /// <summary>
+        /// Get many movements by a filter parameter
+        /// </summary>
+        /// <param name="filter"></param>
+        /// <returns></returns>
         [HttpGet]
         [Route("GetByFilter")]
         [ProducesResponseType(typeof(GetMovementResponse), (int)HttpStatusCode.OK)]
@@ -71,6 +89,11 @@
             }
         }
 
+        /// <summary>
+        /// Get all existing movements on the system
+        /// </summary>
+        /// <param name="filter"></param>
+        /// <returns></returns>
         [HttpGet]
         [Route("GetAll")]
         [ProducesResponseType(typeof(GetMovementResponse), (int)HttpStatusCode.OK)]
@@ -100,6 +123,11 @@
             }
         }
 
+        /// <summary>
+        /// Add a new movement
+        /// </summary>
+        /// <param name="data"></param>
+        /// <returns></returns>
         [HttpPost]
         [Route("")]
         [ProducesResponseType(typeof(Guid?), (int)HttpStatusCode.Created)]
